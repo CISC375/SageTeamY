d297c2bdb4a15dcca6c1814badeae9965040f702
TigerAttack302 <sonicm302@gmail.com>
2025-10-22 12:56:06 -0400

    Fixed imports

--- src/commands/fun/blackjack.ts
+++ src/commands/fun/blackjack.ts
@@ -1,9 +1,11 @@
 import { Command } from '@lib/types/Command';
+import { ChatInputCommandInteraction, InteractionResponse } from 'discord.js';
 
 export default class extends Command {
 
 	description = '';
 	extendedHelp = '';
+	
 
 	async run(interaction: ChatInputCommandInteraction): Promise<InteractionResponse<boolean> | void> {
 
